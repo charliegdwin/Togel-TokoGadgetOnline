76fea0ec8a95b2fba5451bee8d347b83c0a83185
Ardiansyah Bisma R <77134838+Mendoan7@users.noreply.github.com>
2022-05-20 08:54:43 +0700

    Update login.php

--- login.php
+++ login.php
@@ -33,11 +33,8 @@ if (isset($_POST['submit'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
- 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
- 
-    <link rel="stylesheet" type="text/css" href="css/style.css">
- 
+    <link rel="stylesheet" type="text/css" href="css/style-login.css">
     <title>Toko Gadget Online</title>
 </head>
 <body>
