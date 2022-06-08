393ebbd6820add32a3b84bb1cf659bed430748fd
Ardiansyah Bisma R <77134838+Mendoan7@users.noreply.github.com>
2022-06-08 10:41:35 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,30 +1,6 @@
 <?php 
- 
-include 'koneksi.php';
- 
-error_reporting(0);
- 
 session_start();
- 
-if (isset($_SESSION['username'])) {
-    header("Location: berhasil_login.php");
-}
- 
-if (isset($_POST['submit'])) {
-    $email = $_POST['email'];
-    $password = md5($_POST['password']);
- 
-    $sql = "SELECT * FROM tb_user WHERE email='$email' AND password='$password'";
-    $validasi = mysqli_query($koneksi, $sql);
-
-    if ($validasi->num_rows > 0) {
-        $row = mysqli_fetch_assoc($validasi);
-        $_SESSION['username'] = $row['userid'];
-        header("Location: berhasil_login.php");
-    } else {
-        echo "<script>alert('Email atau Password Salah. Coba lagi !')</script>";
-    }
-}
+include 'koneksi.php';
  
 ?>
  
@@ -35,27 +11,45 @@ if (isset($_POST['submit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="css/style-login.css">
-    <title>Toko Gadget Online</title>
+    <title>Login | Toko Gadget Online</title>
 </head>
 <body>
-    <div class="alert alert-warning" role="alert">
-        <?php echo $_SESSION['error']?>
-    </div>
  
     <div class="container">
         <form action="" method="POST" class="login-email">
-            <p class="login-text" style="font-size: 2rem; font-weight: 800;">Login Togel</p>
+            <p class="login-text" style="font-size: 2rem; font-weight: 800;">Login Admin
+            </p>
+            <img class="logo" src="images/logo_long.png">
             <div class="input-group">
-                <input type="email" placeholder="Email" name="email" value="<?php echo $email; ?>" required>
+                <input type="text" name="user" placeholder="Username" class="input-control" required>
             </div>
             <div class="input-group">
-                <input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password']; ?>" required>
+                <input type="password" name="pass" placeholder="Password" required>
             </div>
             <div class="input-group">
                 <button name="submit" class="btn">Login</button>
             </div>
-            <p class="login-register-text">Anda belum punya akun? <a href="form.php">Register</a></p>
         </form>
+        <?php 
+        if (isset($_POST['submit'])) {
+
+            //mysqli_real_escape_string($koneksi agar sql tidak rusak ketika dimasukkan karakter seperti tanda kutip bintang dll
+            $user = mysqli_real_escape_string($koneksi, $_POST['user']);
+            $pass = mysqli_real_escape_string($koneksi, $_POST['pass']);
+         
+            $cek = mysqli_query($koneksi, "SELECT * FROM tabel_admin WHERE username = '".$user."' AND password = '".md5($pass)."'");
+            //mencocokkan apabila user dan password sama dengan yang ada didatabase maka akan keluar angka 1 jika tidak ada maka 0
+            if( mysqli_num_rows($cek) > 0){
+                            $d = mysqli_fetch_object($cek);
+                            $_SESSION['status_login'] = true;
+                            $_SESSION['para_admin'] = $d;
+                            $_SESSION['id'] = $d->admin_id;
+                            echo '<script>window.location="dashboard.php"</script>';
+                        }else{
+                            echo '<script>alert("Username dan Password Salah!")</script>';
+                        }
+}
+ ?>
     </div>
 </body>
 </html>
